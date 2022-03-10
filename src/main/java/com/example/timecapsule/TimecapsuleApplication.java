87352b77bfb86ad1a56367b0c8b1fa4944fa6c44
jhdl0157 <jhdl0157@naver.com>
2022-03-10 16:16:20 +0900

    initcommit

---
+++ src/main/java/com/example/timecapsule/TimecapsuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.timecapsule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimecapsuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimecapsuleApplication.class, args);
+    }
+
+}
