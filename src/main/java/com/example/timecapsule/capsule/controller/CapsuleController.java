78d2bae7a9aee6dfc9e7989892c06638f88792b2
jhdl0157 <jhdl0157@naver.com>
2022-03-17 17:21:34 +0900

    feat: Add CapsuleDomain

---
+++ src/main/java/com/example/timecapsule/capsule/controller/CapsuleController.java
@@ -0,0 +1,13 @@
+package com.example.timecapsule.capsule.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CapsuleController {
+    public ResponseEntity<> postCapsule(){
+
+    }
+}
