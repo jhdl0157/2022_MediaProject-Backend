4b0047db735d0b74ec303c9b1ca07a5f92c2567e
zogak <zogak7@gmail.com>
2022-05-16 17:51:47 +0900

    merge self-login & develop

---
+++ src/main/java/com/example/timecapsule/search/controller/SearchController.java
@@ -0,0 +1,34 @@
+package com.example.timecapsule.search.controller;
+
+import com.example.timecapsule.main.common.ListResult;
+import com.example.timecapsule.main.common.service.ResponseService;
+import com.example.timecapsule.search.dto.UserSearchResponseDto;
+import com.example.timecapsule.search.service.SearchService;
+import com.example.timecapsule.user.dto.response.UserResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@Slf4j
+//@RequestMapping("/api/search")
+public class SearchController {
+    private SearchService searchService;
+    private ResponseService responseService;
+
+    @GetMapping("/api/search")
+    public ResponseEntity<ListResult<UserSearchResponseDto>> getUsersByNicknameKeyword(@RequestParam String keyword){
+        log.info("entered controller");
+        List<UserSearchResponseDto> usersWithKeyword = searchService.getAllUsersWithKeyword(keyword);
+        return new ResponseEntity<>(responseService.getListResult(usersWithKeyword), HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/example/timecapsule/search/service/SearchService.java
@@ -0,0 +1,36 @@
+package com.example.timecapsule.search.service;
+
+import com.example.timecapsule.search.dto.UserSearchResponseDto;
+import com.example.timecapsule.user.entity.User;
+import com.example.timecapsule.user.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@AllArgsConstructor
+@NoArgsConstructor
+@Service
+public class SearchService {
+    @Autowired
+    private UserRepository userRepository;
+    private UserSearchResponseDto userSearchResponseDto;
+
+    public List<UserSearchResponseDto> getAllUsersWithKeyword(String keyword){
+        log.info("entered service");
+        List<User> allUser = userRepository.findAll();
+        List<UserSearchResponseDto> returnList = new ArrayList<UserSearchResponseDto>();
+        for (User user : allUser){
+            if (user.getUserNickname().contains(keyword)) {
+                returnList.add(userSearchResponseDto.toUserSearchResponseDto(user));
+            }
+        }
+        return returnList;
+    }
+
+}
